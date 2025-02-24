c721e8fdc7e6433736576faa38c01400ccab67c9
Corinna <corinna.jaeger@stud.fh-campuswien.ac.at>
2025-02-24 12:06:59 +0100

    Genres added as Label

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -1,17 +1,21 @@
 package at.ac.fhcampuswien.fhmdb.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
     private String title;
     private String description;
+    private List<Genre> genres;
     // TODO add more properties here
 
-    public Movie(String title, String description) {
+    public Movie(String title, String description, List<Genre> genres) {
         this.title = title;
         this.description = description;
+        this.genres = genres;
     }
+
 
     public String getTitle() {
         return title;
@@ -21,10 +25,35 @@ public class Movie {
         return description;
     }
 
-    public static List<Movie> initializeMovies(){
-        List<Movie> movies = new ArrayList<>();
-        // TODO add some dummy data here
-
-        return movies;
+    public String getGenres() {
+        return genres.toString();
     }
-}
+
+    @Override
+    public String toString() {
+        return "\n" + title + "\n" + description + "\nGenres: " + genresToString();
+    }
+
+    private String genresToString() {
+        return genres == null || genres.isEmpty()
+                ? "No genres available"
+                : String.join(", ", genres.stream().map(Enum::name).toList());
+
+    }
+
+        public static List<Movie> initializeMovies() {
+            List<Movie> movies = new ArrayList<>();
+
+            movies.add(new Movie("Inception",
+                    "A thief who enters the dreams of others to steal their secrets must plant an idea in a target’s subconscious.",
+                    Arrays.asList(Genre.SCIENCE_FICTION, Genre.THRILLER, Genre.ACTION)));
+
+            // TODO add some dummy data here
+
+            return movies;
+        }
+
+    }
+
+
+
