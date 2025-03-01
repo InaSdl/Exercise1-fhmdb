6216188cdf81d5d24b60459970483bc77f16112f
Corinna <corinna.jaeger@stud.fh-campuswien.ac.at>
2025-03-01 14:47:34 +0100

    With Dummy data

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -48,6 +48,39 @@ public class Movie {
                     "A thief who enters the dreams of others to steal their secrets must plant an idea in a target’s subconscious.",
                     Arrays.asList(Genre.SCIENCE_FICTION, Genre.THRILLER, Genre.ACTION)));
 
+            movies.add(new Movie("Interstellar",
+                    "A group of explorers embarks on a space journey to find a new habitable planet as Earth faces extinction.",
+                    Arrays.asList(Genre.SCIENCE_FICTION, Genre.DRAMA, Genre.THRILLER)));
+
+            movies.add(new Movie("The Matrix",
+                    "A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers.",
+                    Arrays.asList(Genre.SCIENCE_FICTION, Genre.ACTION, Genre.THRILLER)));
+
+            movies.add(new Movie("Shutter Island",
+                    "A U.S. Marshal investigates the disappearance of a murderer who escaped from a mental institution on a remote island.",
+                    Arrays.asList(Genre.THRILLER, Genre.MYSTERY, Genre.DRAMA)));
+
+            movies.add(new Movie("Memento",
+                    "A man with short-term memory loss attempts to solve his wife’s murder, using tattoos and Polaroid pictures to piece together the clues.",
+                    Arrays.asList(Genre.THRILLER, Genre.MYSTERY, Genre.DRAMA)));
+
+            movies.add(new Movie("The Prestige",
+                    "Two magicians engage in a bitter rivalry, using increasingly dangerous tricks and illusions to outdo each other.",
+                    Arrays.asList(Genre.DRAMA, Genre.MYSTERY, Genre.THRILLER)));
+
+            movies.add(new Movie("Ex Machina",
+                    "A young programmer is selected to participate in a groundbreaking experiment in artificial intelligence by evaluating the human qualities of a highly advanced humanoid A.I.",
+                    Arrays.asList(Genre.SCIENCE_FICTION, Genre.THRILLER, Genre.DRAMA)));
+
+            movies.add(new Movie("Edge of Tomorrow",
+                    "A public relations officer is caught in a time loop and must relive the same day repeatedly while fighting aliens in an intergalactic war.",
+                    Arrays.asList(Genre.SCIENCE_FICTION, Genre.ACTION, Genre.THRILLER)));
+
+            movies.add(new Movie("Total Recall",
+                    "A man’s memories are erased and he finds himself caught up in a battle to escape a conspiracy involving a fictional memory implant service.",
+                    Arrays.asList(Genre.SCIENCE_FICTION, Genre.ACTION, Genre.THRILLER)));
+
+
             // TODO add some dummy data here
 
             return movies;
